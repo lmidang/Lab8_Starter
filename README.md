36406393127e74131b2a9463e16cbd8a9fa9e7b0
lmidang <starrydough@yahoo.com>
2021-11-12 00:57:33 -0800

    added explore section

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Lab 8 - Starter
 Lynn Dang
 
-1. I would fit the automated tests for the Recipe proejct within a Github action that runs every time the code is pushed. This way it ensures that tests are run and working. It would be good to be able to manually run some locally just as a sanity check for the programmer, but the most important point is to run the tests before pushing the code into the main branch where it will be deployed.
-2. An end to end test would be used to check if a function is running correctly given the user input.
+1. I would fit the automated tests for the Recipe project within a Github action that runs every time the code is pushed. This way it ensures that tests are run and working. It would be good to be able to manually run some locally just as a sanity check for the programmer, but the most important point is to run the tests before pushing the code into the main branch where it will be deployed.
+2. An end to end test isn't really meant to check if a function is working properly. Rather, it emulates the user input and checks if different functions work together so that the web page displays the correct thing and the backend is working as expected.
+3. I wouldn't use a unit test to test a "message" feature of a messaging application because this would involve multiple functions from taking in the input from the user to storing that message and then sending it to the other user and displaying it for them. This would be more of an E2E test.
+4. A unit test could test the "max message length" as this would be a single function checking the text input box.
+
+sum.test.js
+![sumtest screenshot](screenshots/sumtest.JPG)
+
+unit.test.js
+![unit test screenshot](screenshots/sumtest.JPG)
+
+lab8.test.js
+![lab8 test screesnhot](screenshots/lab8test.JPG)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,82 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('Check if (234)582-3938 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("(234)582-3938")).toBe(true);
+});
+
+test('Check if (234)591-8539 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("(234)591-8539")).toBe(true);
+});
+
+test('Check if 23459328539 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber(23459328539)).toBe(false);
+});
+
+test('Check if 2345@$#9328539 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber("2345@$#9328539")).toBe(false);
+});
+
+test('Check if yourname@gmail.com is an email', () => {
+    expect(functions.isEmail("yourname@gmail.com")).toBe(true);
+});
+
+test('Check if yourface@exe.edu is an email', () => {
+    expect(functions.isEmail("yourface@exe.edu")).toBe(true);
+});
+
+test('Check if so7e3#@exe.edu is not an email', () => {
+    expect(functions.isEmail("so7e3#@exe.edu")).toBe(false);
+});
+
+test('Check if m3k2$#@exe.edu is not an email', () => {
+    expect(functions.isEmail("m3k2$#@exe.edu")).toBe(false);
+});
+
+test('Check if asdfjkl is a strong password', () => {
+    expect(functions.isStrongPassword("asdfjkl")).toBe(true);
+});
+
+test('Check if ABE8wd is a strong password', () => {
+    expect(functions.isStrongPassword("ABE8wd")).toBe(true);
+});
+
+test('Check if ABE8 d# is not a strong password', () => {
+    expect(functions.isStrongPassword("ABE8 d#")).toBe(false);
+});
+
+test('Check if 3d# is not a strong password', () => {
+    expect(functions.isStrongPassword("3d#")).toBe(false);
+});
+
+test('Check if 12/20/5323 is a date', () => {
+    expect(functions.isDate("12/20/5323")).toBe(true);
+});
+
+test('Check if 03/19/2933 is a date', () => {
+    expect(functions.isDate("03/19/2933")).toBe(true);
+});
+
+test('Check if 03/39/2933 is not a date', () => {
+    expect(functions.isDate("03/39/A33")).toBe(false);
+});
+
+test('Check if 23/39/ze933 is not a date', () => {
+    expect(functions.isDate("23/39/ze933")).toBe(false);
+});
+
+test('Check if #123456 is a hex color', () => {
+    expect(functions.isHexColor("#123456")).toBe(true);
+});
+
+test('Check if #E2A4E6 is a hex color', () => {
+    expect(functions.isHexColor("#E2A4E6")).toBe(true);
+});
+
+test('Check if #E2A4E6wer isnt a hex color', () => {
+    expect(functions.isHexColor("#E2A4E6wer")).toBe(false);
+});
+
+test('Check if # isnt a hex color', () => {
+    expect(functions.isHexColor("#")).toBe(false);
+});
